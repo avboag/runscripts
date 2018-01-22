8db1729a7cc2aae1e08a2975346df0027f358827
Aviel Boag <avboag@gmail.com>
2018-01-22 17:36:38 +0200

    Added support for multiple levels of subdirectories

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import glob
 import os
 import shutil
 import subprocess
@@ -8,18 +9,17 @@ import time
 import yaml
 
 with open(sys.argv[1], 'r') as f:
-    params = yaml.load(f)
+    params = yaml.safe_load(f)
 
-executioner = str(params['executioner'])
-executable = str(params['executable'])
-preamble = str(params['preamble'])
+executioner = params['executioner']
+
 try:
     sleep_time = int(params['sleep_time'])
 except KeyError:
     sleep_time = 1
 
 def count_running():
-    return len([None for d in os.listdir() if os.path.isfile(d + '/queued') or os.path.isfile(d + '/running')])
+    return len(glob.iglob('**/queued')) + len(glob.iglob('**/running'))
 
 max_num_active_processes = int(params['max_num_active_processes'])
 
@@ -33,20 +33,20 @@ mv queued running
 ../{executable} > out 2> error
 
 rm running
-""".format(**globals())
+""".format(params)
 
 with open('script', 'w') as f:
     f.write(script)
 
 os.chmod('script', 0o700)
 
-for d in os.listdir():
-    if os.path.isfile(d + '/planned'):
-        print(d)
-        while count_running() >= max_num_active_processes:
-            print('{} jobs running, sleeping'.format(count_running()))
-            time.sleep(sleep_time)
+for plan_indicator in glob.iglob('**/planned'):
+    d = os.path.dirname(plan_indicator)
+    print(d)
+    while count_running() >= max_num_active_processes:
+        print('{} jobs running, sleeping'.format(count_running()))
+        time.sleep(sleep_time)
 
-        os.rename(d + '/planned', d + '/queued')
+    os.rename(plan_indicator, d + '/queued')
 
-        subprocess.Popen("cd {d}; {executioner} ../script".format(**globals()), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    subprocess.Popen("cd {d}; {executioner} ../script".format(**globals()), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
