97e871bb2c0ad61524606fe5fc7ec0958f10bf50
Aviel Boag <avboag@gmail.com>
2018-01-24 16:50:45 +0200

    Bug fix

--- plan.py
+++ plan.py
@@ -3,9 +3,9 @@ import os
 def simple_id(d, keys):
     return '_'.join([key + '_' + str(d[key]) for key in keys])
 
-def plan(dirname, content):
+def plan(dirname, content, relative_filename = 'param.yaml'):
     os.makedirs(dirname, exist_ok = True)
-    with open(dirname + '/param.yaml', 'w') as f:
+    with open(dirname + '/' + relative_filename, 'w') as f:
         f.write(content)
 
     with open(dirname + '/planned', 'w') as f:
