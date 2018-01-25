193e25f8f5528105dc64c0cf8603baf3049e9064
Aviel Boag <avboag@gmail.com>
2018-01-25 19:05:42 +0200

    Added support for absolute filename in plan

--- plan.py
+++ plan.py
@@ -3,9 +3,9 @@ import os
 def simple_id(d, keys):
     return '_'.join([key + '_' + str(d[key]) for key in keys])
 
-def plan(dirname, content, relative_filename = 'param.yaml'):
+def plan(dirname, content, filename = 'param.yaml'):
     os.makedirs(dirname, exist_ok = True)
-    with open(dirname + '/' + relative_filename, 'w') as f:
+    with open(os.path.join(dirname, filename), 'w') as f:
         f.write(content)
 
     with open(dirname + '/planned', 'w') as f:
